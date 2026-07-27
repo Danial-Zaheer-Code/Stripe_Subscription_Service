951a2a62633127feecafc621d72717c5add02aa2
Danial-Zaheer-Code <danialzaheer80@gmail.com>
2026-07-27 16:25:29 +0500

    Added webhook for stripe.

--- src/controllers/stripeController.js
+++ src/controllers/stripeController.js
@@ -7,3 +7,39 @@ export async function paySubscription(req, res) {
 
     return res.status(result.status).json(result.responseBody);
 }
+
+export async function stripeWebhook(req, res) {
+
+    const signature = req.headers["stripe-signature"];
+
+    const event = stripe.webhooks.constructEvent(
+        req.body,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+    );
+
+    switch (event.type) {
+
+        case "checkout.session.completed":
+
+            const session = event.data.object;
+
+            console.log(session.metadata.userId);
+
+            break;
+
+        case "invoice.paid":
+
+            console.log("Subscription renewed");
+
+            break;
+
+        case "customer.subscription.deleted":
+
+            console.log("Subscription cancelled");
+
+            break;
+    }
+
+    res.sendStatus(200);
+}
