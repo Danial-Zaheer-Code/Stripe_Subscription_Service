16c2ab8049583dcd9ef90962d3592320c0123e86
Danial-Zaheer-Code <danialzaheer80@gmail.com>
2026-08-06 12:10:46 +0500

    Fixed a small type.

--- src/services/couponServices.js
+++ src/services/couponServices.js
@@ -110,7 +110,7 @@ export async function getUserCoupons(userId) {
         const userCoupons = await prisma.userCoupon.findMany({
             where: {
                 userId: userId,
-                isUsed: {
+                count: {
                     gt: 0
                 }
             },
